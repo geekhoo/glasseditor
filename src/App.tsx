0d6f691af62dba6315ca240344bf895286acf2eb
geekhoo <53191460+geekhoo@users.noreply.github.com>
2024-02-14 13:58:11 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EditorComponent from './MEditorComponent';
+//import { Editor } from 'react-draft-wysiwyg';
+//import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+      <div>
+        <EditorComponent html='' id='editor' css=''  />
+      </div>
+
+
+    
+     
     </div>
-  );
+ 
+ );
 }
 
 export default App;
---
+++ src/MEditorComponent.tsx
@@ -0,0 +1,49 @@
+import React,{ useEffect, useState }  from 'react';
+import grapesjs from 'grapesjs';
+
+          
+interface GrapeJSComponentProps {
+    id: string;
+    html: string;
+    css: string;
+}
+
+const EditorComponent: React.FC<GrapeJSComponentProps> = ({ id, html, css }) => {
+    const editorRef = React.useRef<HTMLDivElement>(null);
+
+    const [htmlContent, setHtmlContent] = useState('');
+
+          useEffect(() => {
+            // Fetch the HTML file content
+            fetch('./block001.html')
+              .then(response => response.text())
+              .then(content => setHtmlContent(content))
+              .catch(error => console.error(error));
+          }, []);
+
+    const [cssContent, setCssContent] = useState('');
+
+          useEffect(() => {
+            // Fetch the HTML file content
+            fetch('./style.css')
+              .then(response => response.text())
+              .then(content => setCssContent(cssContent))
+              .catch(error => console.error(error));
+          }, []);
+
+    React.useEffect(() => {
+        const editor = grapesjs.init({
+            container: `#${id}`, // Replace '${id}' with `#${id}`
+            components: htmlContent,
+            style: css,
+        });
+
+        return () => {
+            editor.destroy();
+        };
+    }, [id, html, css, htmlContent, cssContent]);
+
+    return <div id={id} ref={editorRef} />;
+};
+
+export default EditorComponent;
